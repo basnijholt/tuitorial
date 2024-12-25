422f60b21ec7b170083b1e03e645d6f8200870fd
Bas Nijholt <bas@nijho.lt>
2024-12-24 23:12:26 -0800

    fix tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -27,7 +27,7 @@ def chapter(example_code, tutorial_steps):
 async def test_app_init(chapter):
     """Test app initialization."""
     app = TutorialApp([chapter])
-    async with app.run_test() as pilot:
+    async with app.run_test():
         assert len(app.chapters) == 1
         assert app.chapters[0] == chapter
         assert app.current_chapter_index == 0
@@ -39,11 +39,11 @@ async def test_next_focus(chapter):
     app = TutorialApp([chapter])
     async with app.run_test() as pilot:
         # Initial state
-        assert app.current_chapter.current_step_index == 0
+        assert app.current_chapter.current_index == 0
 
-        # Press right arrow
-        await pilot.press("right")
-        assert app.current_chapter.current_step_index == 1
+        # Press down arrow
+        await pilot.press("down")
+        assert app.current_chapter.current_index == 1
 
 
 @pytest.mark.asyncio
@@ -52,15 +52,15 @@ async def test_previous_focus(chapter):
     app = TutorialApp([chapter])
     async with app.run_test() as pilot:
         # Initial state
-        assert app.current_chapter.current_step_index == 0
+        assert app.current_chapter.current_index == 0
 
         # Move to last step
-        app.current_chapter.current_step_index = len(chapter.steps) - 1
-        assert app.current_chapter.current_step_index == 1
+        app.current_chapter.current_index = len(chapter.steps) - 1
+        assert app.current_chapter.current_index == 1
 
-        # Press left arrow
-        await pilot.press("left")
-        assert app.current_chapter.current_step_index == 0
+        # Press up arrow
+        await pilot.press("up")
+        assert app.current_chapter.current_index == 0
 
 
 @pytest.mark.asyncio
@@ -69,11 +69,11 @@ async def test_reset_focus(chapter):
     app = TutorialApp([chapter])
     async with app.run_test() as pilot:
         # Move to last step
-        chapter.current_step_index = len(chapter.steps) - 1
+        chapter.current_index = len(chapter.steps) - 1
 
         # Press reset key
         await pilot.press("r")
-        assert chapter.current_step_index == 0
+        assert chapter.current_index == 0
 
 
 @pytest.mark.asyncio
@@ -94,7 +94,7 @@ async def test_quit():
 async def test_update_display(chapter):
     """Test display updates."""
     app = TutorialApp([chapter])
-    async with app.run_test() as pilot:
+    async with app.run_test():
         initial_description = app.query_one("#description").render()
 
         # Move to next step
@@ -108,7 +108,7 @@ async def test_update_display(chapter):
 async def test_current_step(chapter):
     """Test current_step property."""
     app = TutorialApp([chapter])
-    async with app.run_test() as pilot:
+    async with app.run_test():
         assert chapter.current_step == chapter.steps[0]
 
 
@@ -116,7 +116,7 @@ async def test_current_step(chapter):
 async def test_current_description(chapter):
     """Test current_description property."""
     app = TutorialApp([chapter])
-    async with app.run_test() as pilot:
+    async with app.run_test():
         assert chapter.current_step.description == chapter.steps[0].description
 
 
@@ -132,26 +132,9 @@ async def test_switch_chapters(chapter, example_code, tutorial_steps):
 
     async with app.run_test() as pilot:
         # Ensure the tabs are mounted
-        await pilot.pause()
-
-        # Switch to the second chapter
-        tab_test_chapter_2 = app.query(f"#{chapter2.title.lower().replace(' ', '-')}")
-        assert tab_test_chapter_2
-        await pilot.click(tab_test_chapter_2.last())
-
-        # Check if the current chapter is updated
+        await pilot.press("right")
         assert app.current_chapter.title == chapter2.title
-
-        # Check if the step is reset to 0
-        assert app.current_chapter.current_step_index == 0
-
-        # Switch back to the first chapter
-        tab_test_chapter = app.query(f"#{chapter.title.lower().replace(' ', '-')}")
-        assert tab_test_chapter
-        await pilot.click(tab_test_chapter.last())
-
-        # Check if the current chapter is updated
+        assert app.current_chapter_index == 1
+        await pilot.press("right")
         assert app.current_chapter.title == chapter.title
-
-        # Check if the step is reset to 0
-        assert app.current_chapter.current_step_index == 0
+        assert app.current_chapter_index == 0
